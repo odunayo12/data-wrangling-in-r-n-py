3f97c0eb8b2868b8e23d15f65cb11b4cfa99a93c
odunayo12 <odun.rotimi@gmail.com>
2020-11-21 21:05:59 +0100

    statring files

---
+++ titanic.py
@@ -0,0 +1,3 @@
+# https://www.kaggle.com/startupsci/titanic-data-science-solutions
+# for pivoting
+# https://pbpython.com/pandas-pivot-table-explained.html
---
+++ titanic_r.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+# data from https://gist.github.com/michhar/2dfd2de0d4f8727f873422c5d959fff5
+titanic_data <- read_csv("data/titanic.csv")
+View(titanic_data)
+
